954904a2e21d8eea3f9813aa26c940ebecd87e05
Yusef Marra <yusef.marra@sling.com>
2018-03-30 11:43:09 -0600

    testing ci

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
 class App extends React.Component {
-  async componentWillMount() {
+  constructor(props) {
+    super(props);
+    this.getNewName = this.getNewName.bind(this);
+  }
+
+  componentWillMount() {
+    this.getNewName();
+  }
+
+  async getNewName() {
     const { data } = await axios.get('/api/name');
     this.setState({ ...data });
   }
 
   render() {
-   return <div>{ this.state ? `${this.state.firstName} ${this.state.lastName}` : null }</div>;
+   return (
+      <div>
+        { this.state ? `${this.state.firstName} ${this.state.lastName}` : null }
+        <button onClick={this.getNewName}>GIT IT</button>
+      </div>
+   );
   }
 }
 
