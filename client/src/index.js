7d6cee2e7e696245057f10d8a7feb1b4a28e7f44
Yusef Marra <yusef.marra@gmail.com>
2018-06-06 15:17:57 -0600

    Delete old client

--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
-
-import App from './components/App';
-
-window.axios = axios;
-
-ReactDOM.render(<App />, document.querySelector('#root'));
